eff60cc12afb65d89f2759923c718f918b8a495e
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-07-05 13:17:14 +0300

    done-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,75 @@
-import { Feedback } from "./feedback/feedback";
+import React from "react";
+import { Component  } from "react";
+import PropTypes from 'prop-types';
+import { Statistics } from "./feedback/Statistics";
+import { FeedbackOptions } from "./feedback/FeedbackOptions";
+import { Section } from "./feedback/Section";
+import { Notification } from "./feedback/Notification";
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        marginLeft: '30px',
-        
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Feedback></Feedback>
-    </div>
-  );
-};
+
+export class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      }
+
+
+      onLeaveFeedback = (stateKey) => {
+        this.setState(prevState => {
+            return ({
+                [stateKey]: prevState[stateKey] + 1
+             })}
+             )
+      }
+
+      countTotalFeedback = () => (
+        Object.values(this.state).reduce((acc, number) => {
+            return acc + number}, 0)
+      )
+
+      countPositiveFeedbackPercentage= () => {
+        const factor = this.countTotalFeedback() / this.state.good;
+        return `${(Math.round(100 / factor))}%`
+      }
+
+      render() {
+        return (
+            <>
+            <Section title="Please leave feedback">
+                
+                <FeedbackOptions 
+                    options={this.state} 
+                    onLeaveFeedback={this.onLeaveFeedback} />
+
+                {this.countTotalFeedback()
+                    ? <Statistics 
+                    allStates={this.state} 
+                    total={this.countTotalFeedback()} 
+                    positivePercentage={this.countPositiveFeedbackPercentage()} />
+                    : <Notification message="There is no feedback"/>
+            
+            }
+            </Section>
+            </>
+        )
+      }
+
+}
+
+
+
+Section.propTypes = {
+    title: PropTypes.string
+}
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.object,
+    onLeaveFeedback: PropTypes.func
+}
+
+Statistics.propTypes = {
+    allStates: PropTypes.object,
+    total: PropTypes.number,
+    positivePercentage: PropTypes.string
+}
--- src/components/feedback/FeedbackOptions.jsx
+++ src/components/feedback/FeedbackOptions.jsx
@@ -1,7 +1,7 @@
 import { BtnListStyled, BtnStyled } from "./feedback.styled";
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-    <>
+
             <BtnListStyled>
                 {Object.keys(options).map(stateKey => (
                     <BtnStyled key={stateKey} onClick={ () => onLeaveFeedback(stateKey)}>{ stateKey }</BtnStyled>
@@ -9,5 +9,5 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
                 }
             </BtnListStyled>
     
-    </>
+
 )
